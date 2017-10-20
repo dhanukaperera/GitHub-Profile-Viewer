3e9b1f287e9986b9d9c1874d446c65aab70bd556
dhanukaperera <dhanukashehanperera@gmail.com>
2017-10-20 07:51:03 +0530

    created the mock repository

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import { SearchResultComponent } from './search-result/search-result.component';
 import { IonicModule } from 'ionic-angular';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
-    declarations:[SearchResultComponent],
+    declarations:[SearchResultComponent,
+    RepositoryComponent],
     imports: [IonicModule],
-    exports: [SearchResultComponent]
+    exports: [SearchResultComponent,
+    RepositoryComponent]
 })
 
 export class ComponentsModule {
---
+++ src/components/repository/repository.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { Repository } from '../../models/repository.interface';
+
+/**
+ * Generated class for the RepositoryComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'repository',
+  templateUrl: 'repository.component.html'
+})
+export class RepositoryComponent {
+
+  @Input() repository: Repository;
+
+
+}
